6313a761f8d723ec4aace430548330e2f97ab179
Nan Wang <nan.wang@conic.ai>
2020-09-23 23:55:04 +0800

    feat: adapt to only building the jekyll

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@
 set -e
 
 DEST="${JEKYLL_DESTINATION:-_site}"
-REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-BRANCH="gh-pages"
 BUNDLE_BUILD__SASSC=--disable-march-tune-native
 
 echo "Installing gems..."
@@ -16,13 +14,15 @@ echo "Building Jekyll site..."
 
 JEKYLL_ENV=production bundle exec jekyll build
 
-echo "Publishing..."
-
 cd ${DEST}
 
 git init
 git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config user.email "${GITHUB_ACTOR_EMAIL}"
+touch .nojekyll
+if [ -z "${CUSTOM_DOMAIN}" ]
+then
+  echo ${CUSTOM_DOMAIN} > CNAME
+fi
 git add .
 git commit -m "published by GitHub Actions"
-git push --force ${REPO} master:${BRANCH}
